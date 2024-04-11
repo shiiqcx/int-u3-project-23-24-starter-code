3a9969fb9d9f0b39ffa5fc72e216b3459e81ca07
shiiqcx <149090282+shiiqcx@users.noreply.github.com>
2024-04-11 15:25:30 +0000

    hTML

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Declare variables below to save the different divs of your story.
+let start = document.querySelector(".start");
 let storyOpening = document.querySelector(".story-opening");
 let optionOneScreen = document.querySelector(".option-one-screen");
 let optionTwoScreen = document.querySelector(".option-two-screen");
@@ -8,6 +9,7 @@ let optionOneEndOne = document.querySelector(".option-one-end-one");
 let optionTwoEnd = document.querySelector(".option-two-end");
 let optionOneEndOneGood = document.querySelector(".option-one-end-one-good");
 
+let enterButton = document.querySelector(".enter-button");
 let yButton = document.querySelector(".yes-button");
 let nButton = document.querySelector(".no-button");
 let helpButton = document.querySelector(".help-man-button");
@@ -19,18 +21,30 @@ let rightButton = document.querySelector(".right-button");
 let followRightButton = document.querySelector(".follow-right-button");
 let leaveLeftButton = document.querySelector(".leave-left-button");
 
+let nameBox = document.querySelector(".name");
+let random = document.querySelector(".random");
+let randomTwo = document.querySelector(".random-2")
+
 
 
 
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
+enterButton.addEventListener('click', function(){
+  storyOpening.style.display = "block";
+  start.style.display = "none";
+  
+});
+
 yButton.addEventListener('click', function(){
   optionOneScreen.style.display="block";
   storyOpening.style.display="none";
+  random.innerHTML = nameBox.value + " enters the cave with a lit torch and stumble upon an injured stranger."
  });
 nButton.addEventListener('click', function(){
   optionTwoScreen.style.display="block";
   storyOpening.style.display="none";
+  randomTwo.innerHTML = nameBox.value + " is forcefully pushed into the cave and falls obtaining -5 health." 
  });
 helpButton.addEventListener('click', function(){
   optionOneScreenTwo.style.display="block";
